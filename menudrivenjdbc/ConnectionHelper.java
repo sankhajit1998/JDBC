e7c298501e1e91472680c179c62a8365ccb9e2e8
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-09-07 18:55:30 +0530

    Update ConnectionHelper.java

--- menudrivenjdbc/ConnectionHelper.java
+++ menudrivenjdbc/ConnectionHelper.java
@@ -1,18 +1,24 @@
+/*
+@Author Sankhajit Roy
+*/
 package com.menudrivenjdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionHelper {
+public class ConnectionHelper {    // class
 	static {
+		// try block
 		try {
 			// register my sql driver
 			Class.forName("com.mysql.cj.jdbc.Driver");
+		// catch block	
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
+	// Coonection method to get connection from sql database
 	public static Connection con () throws SQLException {
 		return DriverManager.getConnection("jdbc:mysql://localhost:3306/lab","root","anit@1998*07");
 	}
