75bad5fc5cac0f91207261cd9db71361dc3f89b1
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-09-07 18:53:36 +0530

    Update Menudriven.java

--- menudrivenjdbc/Menudriven.java
+++ menudrivenjdbc/Menudriven.java
@@ -1,3 +1,6 @@
+/*
+@Author Sankhajit
+*/
 package com.menudrivenjdbc;
 
 import java.sql.Connection;
@@ -7,8 +10,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class Menudriven {
-	
+public class Menudriven {   // class
+	// created a scanner objects
 	Scanner s = new Scanner(System.in);
 	Scanner s1 = new Scanner(System.in);
 	Scanner s2 = new Scanner(System.in);
@@ -20,8 +23,9 @@ public class Menudriven {
 	int id, age, salary;
 	String name,city;
 	
-	// saving details in database
+	// method for saving details in database
 	public void saveMenudriven() throws SQLException {
+		// take user input
 		System.out.println("Enter Employee ID: ");
 		id = s1.nextInt();
 		System.out.println("Enter Employee Name: ");
@@ -32,45 +36,63 @@ public class Menudriven {
 		city = s4.next();
 		System.out.println("Enter Employee Salary: ");
 		salary = s5.nextInt();
-		
+		// create a ConnectionHelper class object
 		Connection conn = ConnectionHelper.con();
+		// declare sql statement to insert value into table
 		PreparedStatement s1 = conn.prepareStatement("insert into menudriven values(?,?,?,?,?)");
+		// set values into table
 		s1.setInt(1, id);
 		s1.setString(2, name);
 		s1.setInt(3, age);
 		s1.setString(4, city);
 		s1.setInt(5, salary);
+		// execute the statement
 		s1.executeUpdate();
 		
 	}
 	
-	// display database
+	// method for display database
 	public void displayMenudriven() throws SQLException {
+		// create a ConnectionHelper class object
 		Connection conn = ConnectionHelper.con();
+		// crated a statement object
 		Statement st = conn.createStatement();
+		// declare sql statement
 		ResultSet rs = st.executeQuery("select * from menudriven");
-		
+		// traversing the table
 		while (rs.next()) {
 			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4)+" "+rs.getInt(5));
 			
 		}
 	}
-	
-	// updating database
-	public void updateMenudriven() throws SQLException {
+
+	// method for updating database
+	public void updateMenudriven() throws SQLException{
+		// create a ConnectionHelper class object
 		Connection conn = ConnectionHelper.con();
+		// crated a statement object
 		Statement st = conn.createStatement();
-		System.out.print("Enter Employee City: ");
-		city = s.nextLine();
+		// take input from user & read the input
+		System.out.println("Enter Employe Id you want to update: ");
+		id = s.nextInt();
+		// take input from user & read the input
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter Employe City: ");
+		city = s1.nextLine();
+		// execute the statement 
 		st.executeUpdate("update Menudriven set city='"+city+"' where Id="+id);
 	}
 	
-	// deleting details from database
+	// method for deleting details from database
 	public void deleteMenudriven() throws SQLException {
+		// create a ConnectionHelper class object
 		Connection conn = ConnectionHelper.con();
+		// crated a statement object
 		Statement st = conn.createStatement();
+		// take input from user & read the input
 		System.out.print("Enter Employee ID: ");
 		id = s.nextInt();
+		// execute the statement
 		st.executeUpdate("delete from menudriven where id = "+id);
 	}
 	
